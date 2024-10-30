d23291c2c5d093b323ab7dafd6cfc284f8d1e5b3
shabman <avia.shabbyman@gmail.com>
2024-10-30 22:06:57 +0000

    Masking signals

--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
 #include "cd.h"
 #include "parse.h"
 
+#include <signal.h>
+
 int
 main ( void )
 {
+  signal ( SIGINT, SIG_IGN );
   pcli_init_cd ();
 
   printf ( "\n%s\n%s\n\n", "Welcome to the PosixCLI!", "Copyright (c) 2024 Mustafa Malik (mm2440@kent.ac.uk). Licensed under GNU GPLv3" );
