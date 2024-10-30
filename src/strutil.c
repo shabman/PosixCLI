8b9886174a1a9694c4664509b0ed17aaf842e820
shabman <avia.shabbyman@gmail.com>
2024-10-30 22:06:41 +0000

    Added strstrip

--- src/strutil.c
+++ src/strutil.c
@@ -55,3 +55,25 @@ strtrim ( char* str )
 
   return str;
 }
+
+char*
+strstrip ( char* s )
+{
+  size_t size;
+  char* end;
+
+  size = strlen ( s );
+
+  if ( !size )
+    return s;
+
+  end = s + size - 1;
+  while ( end >= s && isspace ( *end ) )
+    end--;
+  *( end + 1 ) = '\0';
+
+  while ( *s && isspace ( *s ) )
+    s++;
+
+  return s;
+}
